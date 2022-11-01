14d2387a3897f29d51c4b53a80a8a8e1eb5e2600
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-01 10:07:19 +0300

    Add project files.

---
+++ objectDegisken/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+object onRenk = ConsoleColor.Green;
+ConsoleColor arkaRenk = ConsoleColor.Red;
+Console.ForegroundColor =(ConsoleColor) onRenk;
+Console.BackgroundColor = arkaRenk;
+
+object x = 100;
+x = Convert.ToInt32(x) + 200;
+object y = 100.25M;
+float z = 100.25f;
+bool deger = true;
+deger = !deger;
+object ad = "Duygu";
+Console.WriteLine("int" + x);
+Console.WriteLine("decimal" + y);
+Console.WriteLine("Mantıksal" + deger);
+Console.WriteLine("Katar" + ad);
+
+
